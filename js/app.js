1d24a5a3f7ce08b32d07c8804de9d5019d7f8095
hlateman <henryspethmann@Henrys-Betamax-2.local>
2016-04-14 20:47:21 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    
+// get the td's in the table to repeat to fill the background
+
+$(function(){
+
+  // listen for a submit event on the #itunes-search form
+  $("#giphy-search").submit(function(event){
+    event.preventDefault();
+
+    // fetch search term from the text field
+    var searchTerm = $("#search-term").val();
+
+    // call search function and pass the search term as a parameter
+    search(searchTerm);
+    $("#results-table").html("");
+    $("#search-term").val("");
+  });
+
+  function displayResults(results){
+
+  	var first = results.data[0];
+
+    $("#results-table tbody")
+      .append("<div><img src='" + first.images.fixed_height.url +"'/></div>");
+
+
+    // use jquery to create a new table row containing data received from the API
+    // $.each(results.data, function(index, gif){
+    //   $("#results-table tbody")
+    //   .append("<tr>" +
+    //           "<td>" + gif.rating + "</td>" +
+    //           "<td><img src='" + gif.images.fixed_height.url +"'/></td>" +
+    //           "</tr>");
+
+    // });
+  }
+
+  function search(term){
+    var url = "http://api.giphy.com/v1/gifs/search";
+    // this is the public beta key, sign up for production if you want this to be live on the web
+    var apiKey = "dc6zaTOxFJmzC";
+
+    // Makes an API request to the itunes server with a request for data using the search term
+    // This uses jQuery's .ajax function
+    $.ajax({
+      url: url,
+      type: "GET",
+      data: {q: term, api_key: apiKey },
+      success: function(data){
+        console.log(data);
+        displayResults(data);
+      }
+    });
+  }
+});
+
+// when user enters word
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
